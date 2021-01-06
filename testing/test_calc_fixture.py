fb9fb5e905b9d4c20742a133d81bf666485feae2
Yocczy <532372163@qq.com>
2021-01-06 22:47:36 +0800

    计算器测试

--- testing/test_calc_fixture.py
+++ testing/test_calc_fixture.py
@@ -1,3 +1,4 @@
+import allure
 import pytest
 import yaml
 from python_code.calc import Calculator
@@ -29,8 +30,12 @@ from python_code.calc import Calculator
 def test(test):
     pass
 
+
+@allure.feature('测试计算器')
 class TestCalc():
 
+    @pytest.mark.add
+    @allure.story('测试加法')
     @pytest.mark.run(order=0)
     def test_add(self, get_calc, get_add_datas):
         result = get_calc.add(get_add_datas[0], get_add_datas[1])
@@ -38,6 +43,7 @@ class TestCalc():
             result = round(result, 2)
         assert result == get_add_datas[2]
 
+    @pytest.mark.div
     @pytest.mark.run(order=3)
     def test_div(self, get_calc, get_div_datas):
         result = get_calc.div(get_div_datas[0], get_div_datas[1])
@@ -45,6 +51,7 @@ class TestCalc():
             result = round(result, 2)
         assert result == get_div_datas[2]
 
+    @pytest.mark.sub
     @pytest.mark.run(order=1)
     def test_sub(self, get_calc, get_sub_datas):
         result = get_calc.sub(get_sub_datas[0], get_sub_datas[1])
@@ -52,6 +59,7 @@ class TestCalc():
             result = round(result, 2)
         assert result == get_sub_datas[2]
 
+    @pytest.mark.mul
     @pytest.mark.run(order=2)
     def test_mul(self, get_calc, get_mul_datas):
         result = get_calc.mul(get_mul_datas[0], get_mul_datas[1])
